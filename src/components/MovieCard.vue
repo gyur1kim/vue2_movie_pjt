be728d55945e07e18c42b2c21a61af174e3af8dd
김규리 <qri1114@gmail.com>
2022-11-11 11:03:41 +0900

    movie card 완성

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <h3>MovieCard</h3>
+    <img :src="poster_path" alt="영화 포스터">
+    <h3>{{ movie.title }}</h3>
+    <p>{{ overview }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MovieCard',
+  data(){
+    return{
+      poster_path: `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${this.movie.poster_path}`,
+      overview: this.movie.overview.slice(0, 100) + '...',
+    }
+  },
+  props: {
+    movie: Object,
+  }
 }
 </script>
 
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="movie">
     <h1>뀰movie</h1>
-    <MovieCard/>
+    <MovieCard 
+      v-for="(movie, idx) in topMovies" :key="idx"
+      :movie="movie"
+    />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import MovieCard from '@/components/MovieCard'
 
 export default {
   name: 'MovieView',
+  data() {
+    return{
+      topMovies: null,
+    }
+  },
   components: {
     MovieCard,
+  },
+  methods: {
+    topRatedMovies(){
+      const URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=2808f49cb7a157269d3c55874b8d52e5&language=ko-KR'
+
+      axios({
+        method: 'GET',
+        url: URL,
+      })
+        .then((response)=>{
+          // console.log(response);
+          // console.log(response.data.results);
+          this.topMovies = response.data.results;
+        })
+        .catch((error)=>{
+          console.log(error);
+        })
+    },
+  },
+  created(){
+    this.topRatedMovies();
   }
 }
 </script>
